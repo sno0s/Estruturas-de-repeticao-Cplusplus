12704e5949d5a5243d80b48dc8ceec7a993a1cba
Matheus Maciel <89057816+sno0s@users.noreply.github.com>
2023-11-19 15:27:43 -0300

    Update Maior e menor.cpp

--- Maior e menor.cpp
+++ Maior e menor.cpp
@@ -3,12 +3,29 @@ using namespace std;
 
 int main()
 {
-	int qi;
-	int n;
-	int s;
-	//"qi" � quantidade inicial
+	int maior, menor; //Variáveis responsáveis por determinar o maior e o menor número
+	int i = 0; //Contador
+	int x, y;
 
-	for
-	
-	cout << s;
+	cout << "Quantos numeros?: ";
+	cin >> x;
+
+	cout << "Insira " << x << " numeros: ";
+	for(i = 0; i < x; i++)
+	{
+		if(i == 0)
+		{
+			maior = x;
+			menor = x;
+		}
+		cin >> y;
+
+		if(y > maior)
+			maior = y;
+		if(y < menor)
+			menor = y;
+	}
+
+	cout << endl << "Maior numero: " << maior << endl;
+	cout << "Menor numero: " << menor << endl;
 }
